5e8679efd9bb61d71c18c49b82f8c9d6b6a9d4a6
root <root@localhost>
2024-11-17 16:44:15 +0800

    auto publish 1731833055444

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<<"Name  :"; cout<<"\t\t";
+  cout<<"Flores Allyssa Jane B.\n";
+  cout<<"Student Number  :"; cout<<"\t\t";
+  cout<<"2024 - 100780 \n";
+  cout<<"Program  :"; cout<<"\t\t";
+  cout<<"BSCE - Bachelor of Science in Civil Engineering \n";
+  cout<<"Course  :"; cout<<"\t\t";
+  cout<<"CPE01 - Computer Fundamentals and Programming \n";
+ return 0;
+}
